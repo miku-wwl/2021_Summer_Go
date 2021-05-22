c4c460dfdaad799f3bdf74066515baa5979d06c1
miku-wwl <569144003@qq.com>
2021-05-22 21:17:22 +0800

    2021/05/22

---
+++ runoob_Advanced/exp1_pointer2.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var  ptr *int  //空指针
+
+	fmt.Printf("ptr 的值为 : %x\n", ptr  )
+}
---
+++ runoob_Advanced/exp3_slice1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers = make([]int,3,5)
+
+	printSlice(numbers)
+}
+
+func printSlice(x []int){
+	fmt.Printf("len=%d cap=%d slice=%v\n",len(x),cap(x),x)
+}
+
+//exp3_slice
